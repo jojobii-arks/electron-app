e1206bd782ce68997814a1e2a1cb5965d69bc529
joseph-bravo <josephangelobravo@gmail.com>
2023-02-18 08:40:19 -0800

    feat: add ipc invoker and handler

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,11 +5,11 @@ export default function App() {
   const [count, setCount] = useState<number>(0);
   return (
     <>
-      <main className="flex h-screen flex-col items-center justify-center">
+      <main className="flex h-screen flex-col items-center justify-center p-16 ">
         <h1 className="text-4xl font-black ">Hello World!</h1>
         <h2 className="m-4 w-fit rounded-xl bg-gray-200 p-4">{count}</h2>
         <button
-          className="btn"
+          className="btn mb-2 "
           onClick={() => {
             setCount(count + 1);
           }}
@@ -23,6 +23,17 @@ export default function App() {
           }}
         >
           Subtract
+        </button>
+        <hr className="my-8 w-full" />
+        <button
+          className="btn"
+          onClick={async () => {
+            //@ts-expect-error electronapi
+            const x = await window.electronapi.doThing();
+            alert(x);
+          }}
+        >
+          Click Me!
         </button>
       </main>
     </>
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// Preload (Isolated World)
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("electronapi", {
+  doThing: async () => await ipcRenderer.invoke("do-a-thing"),
+});
